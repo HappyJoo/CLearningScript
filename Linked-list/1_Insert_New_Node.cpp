afb86f895f0c82216c4e81feab0b428c4eaddc59
happyjoo <happy_joo@163.com>
2020-02-29 07:59:24 +0800

    update Linked-list

---
+++ Linked-list/1_Insert_New_Node.cpp
@@ -0,0 +1,14 @@
+struct Node *insert(struct Node *head, int ind, struct Node *a) {
+    struct Node ret, *p = &ret;
+    ret.next = head;
+    while (ind--) p = p->next;
+    a->next = p->next;
+    p->next = a;
+    return ret.next;
+}
+
+// To help you understand haha~
+----p----p.next----|    ---->   |----p     p.next----
+                   |   ---->   |     \   /
+                   |   ---->   |      \ /
+-------a-----------|   ---->   |       a
