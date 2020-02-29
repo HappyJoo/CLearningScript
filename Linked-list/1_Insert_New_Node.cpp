ba9006bb9b3ec7602ba57a6521214a88c3059d1d
happyjoo <happy_joo@163.com>
2020-02-29 08:04:33 +0800

    update

--- Linked-list/1_Insert_New_Node.cpp
+++ Linked-list/1_Insert_New_Node.cpp
@@ -8,7 +8,8 @@ struct Node *insert(struct Node *head, int ind, struct Node *a) {
 }
 
 // To help you understand haha~
-----p----p.next----|    ---->   |----p     p.next----
-                   |   ---->   |     \   /
-                   |   ---->   |      \ /
--------a-----------|   ---->   |       a
+// Just imagine cut and reconnect lines.
+// ----p----p.next----|    ---->   |----p     p.next----
+//                    |   ---->   |     \   /
+//                    |   ---->   |      \ /
+// -------a-----------|   ---->   |       a
